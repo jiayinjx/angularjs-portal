f7b2ef063ccba4aec074dc59841b3dca0bf7cc3e
Jianyi Liu <jiayinliuao@gmail.com>
2015-08-25 16:16:48 -0500

    MUMUP-2011 home page (i) tips broke in XS sizes.

--- angularjs-portal-home/src/main/webapp/my-app/layout/controllers.js
+++ angularjs-portal-home/src/main/webapp/my-app/layout/controllers.js
@@ -266,4 +266,153 @@ define(['angular', 'jquery'], function(angular, $) {
 
     return app;
 
+
+    // replacement position factory for tooltip fix
+    angular.module('ui.bootstrap.position',[]).factory('$position', ['$document', '$window', function ($document, $window) {
+        function getStyle(el, cssprops) {
+            if (el.currentStyle) { //IE
+                return el.currentStyle[cssprops];
+            } else if ($window.getComputedStyle) {
+                return $window.getComputedStyle(el)[cssprops];
+            }
+            // finally try and get inline style
+            return el.style[cssprops];
+        }
+
+        /**
+         * Checks if a given element is statically positioned
+         * @param element - raw DOM element
+         */
+        function isStaticPositioned(element) {
+            return (getStyle(element, 'position') || 'static' ) === 'static';
+        }
+
+        /**
+         * returns the closest, non-statically positioned parentOffset of a given element
+         * @param element
+         */
+        var parentOffsetEl = function (element) {
+            var docDomEl = $document[0];
+            var offsetParent = element.offsetParent || docDomEl;
+            while (offsetParent && offsetParent !== docDomEl && isStaticPositioned(offsetParent) ) {
+                offsetParent = offsetParent.offsetParent;
+            }
+            return offsetParent || docDomEl;
+        };
+
+        return {
+        /*
+         * Privides read-only equivalent of jQuery's position function:
+         * http://api.jquery.com/position/
+         */
+            position: function (element) {
+                var elBCR = this.offset(element);
+                var offsetParentBCR = { top: 0, left: 0 };
+                var offsetParentEl = parentOffsetEl(element[0]);
+                if (offsetParentEl != $document[0]) {
+                 offsetParentBCR = this.offset(angular.element(offsetParentEl));
+                 offsetParentBCR.top += offsetParentEl.clientTop - offsetParentEl.scrollTop;
+                 offsetParentBCR.left += offsetParentEl.clientLeft - offsetParentEl.scrollLeft;
+                }
+
+                var boundingClientRect = element[0].getBoundingClientRect();
+
+                return {
+                 width: boundingClientRect.width || element.prop('offsetWidth'),
+                 height: boundingClientRect.height || element.prop('offsetHeight'),
+                 top: elBCR.top - offsetParentBCR.top,
+                 left: elBCR.left - offsetParentBCR.left
+                };
+            },
+
+        /*
+         * Provides read-only equivalent of jQuery's offset function:
+         * http://api.jquery.com/offset/
+         */
+            offset: function (element) {
+                var boundingClientRect = element[0].getBoundingClientRect();
+                return {
+                    width: boundingClientRect.width || element.prop('offsetWidth'),
+                    height: boundingClientRect.height || element.prop('offsetHeight'),
+                    top: boundingClientRect.top + ($window.pageYOffset || $document[0].documentElement.scrollTop),
+                    left: boundingClientRect.left + ($window.pageXOffset || $document[0].documentElement.scrollLeft)
+                };
+            },
+
+        /*
+         * Provides coordinates for the targetEl in relation to hostEl
+         */
+             positionElement: function (hostEl, targetEl, positionStr, appendToBody) {
+                var positionStrParts = positionStr.split('-');
+                var pos0 = positionStrParts[0], pos1 = positionStrParts[1] || 'center';
+
+                var hostElPos, targetElWidth, targetElHeight, targetElPos, windowEl;
+                hostElPos = appendToBody ? this.offset(hostEl) : this.position(hostEl);
+                targetElWidth = targetEl.prop('offsetWidth');
+                targetElHeight = targetEl.prop('offsetHeight');
+                windowEl = {width: $window.outerWidth };
+
+                var shiftWidth = {
+                    center: function() {
+                        var toReturn = hostElPos.left + hostElPos.width / 2 - targetElWidth / 2;
+                        // This is the fix for running off the sides of the Screens
+                        if (appenToBody) {
+                            var targetElRight = toReturn + targetElWidth;
+                            //if the host is within the window but a negative
+                            if (toReturn < 0) {
+                                targetEl.addClass("top-offset-left");
+                                toReturn = 0;
+                            }
+                        }
+                        return toReturn;
+                    },
+                    left: function() {
+                        return hostElPos.left;
+                    },
+                    right: function() {
+                        return hostElPos.left + hostElPos.width;
+                    }
+                };
+
+                var shiftHight = {
+                    center: function () {
+                                return hostElPos.top + hostElPos.height / 2 - targetElHeight / 2;
+                    },
+                    top: function () {
+                        return hostElPos.top;
+                    },
+                    bottom: function () {
+                        return hostElPos.top + hostElPos.height;
+                    }
+                };
+                switch (pos0) {
+                    case 'right':
+                        targetElPos = {
+                            top: shiftHeight[pos1](),
+                            left: shiftWidth[pos0]()
+                        };
+                        break;
+                    case 'left':
+                        targetElPos = {
+                            top: shiftHeight[pos1](),
+                            left: hostElPos.left - targetElWidth
+                        };
+                        break;
+                    case 'bottom':
+                        targetElPos = {
+                            top: shiftHeight[pos0](),
+                            left: shiftWidth[pos1]()
+                        };
+                        break;
+                    default:
+                        targetElPos = {
+                            top: hostElPos.top - targetElHeight,
+                            left: shiftWidth[pos1]()
+                        };
+                        break;
+                    }
+                    return targetElPos;
+                }
+            };
+    }]);
 });
